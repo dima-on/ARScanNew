33614f21e1f849a2bfcefdbd82b3da9e63496bd6
dima-on <67599997+dima-on@users.noreply.github.com>
2024-03-05 20:01:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -59,5 +59,5 @@ def StartAll():
     Down_Image_Path = WorkFile.Down_Image_Path
     return jsonify({'result_image': Top_Image_Path, 'result_imageD': Down_Image_Path})
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=443, ssl_context=context)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=443, ssl_context=context)
+    #app.run(host='0.0.0.0', port=5000)
