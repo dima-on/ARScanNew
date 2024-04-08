ec3b20330297dbd4f159d03adecd0a7510100950
dima-on <67599997+dima-on@users.noreply.github.com>
2024-04-09 00:16:36 +0300

    1.5

--- WorkFile.py
+++ WorkFile.py
@@ -8,41 +8,8 @@ import requests
 from io import BytesIO
 
 
-Down_Image_Path = ['static/Photo/Down/legs.png', 'static/Photo/Down/leggins-1.png', 'static/Photo/Down/Down.png']
-Top_Image_Path = ['static/Photo/Top/T_Short_Red.png', 'static/Photo/Top/T_Short_EnerGym.png', 'static/Photo/Top/Coft-removebg-preview.png', 'static/Photo/Top/top-1.png', 'static/Photo/Top/te.png', "static/Photo/Top/Top.png"]
-
-Top_Offset_To_Y = [15, 2.8, 30, 13, 8, 20]
-Top_Offset_To_X = [-2, -1, -4, -3, -4, 0]
-
-Top_With = [1150, 1300, 1280, 800, 1150, 1100]
-Top_HeightMult = [-0.15, -0.15, 0, -0.15, -0.25, -0.15]
-
-Down_Offset_To_Y = [-30, -10, -100]
-Down_Offset_To_X = [0, -1, 0]
-
-Down_With = [1250, 1100, 1000]
-Down_HeightMult = [0, 0, -0.2]
-
-Down_overlay_image = []
-Top_overlay_image = []
 
 
-
-def BeginFun():
-
-
-    Count_For = 0
-    for im in Down_Image_Path:
-        Down_overlay_image.append(1)
-        Down_overlay_image[Count_For] = Image.open(im)
-        Count_For += 1
-
-
-    Count_For = 0
-    for im in Top_Image_Path:
-        Top_overlay_image.append(1)
-        Top_overlay_image[Count_For] = Image.open(im)
-        Count_For += 1
 
 
 mp_pose = mp.solutions.pose
@@ -59,6 +26,7 @@ def get_image_size(url):
     # Получение размеров изображения
     width, height = image.size
     return width, height
+
 def body_Detect(Index, frame, landmarks, array):
     ySize, xSize = frame.shape[:2]
 
